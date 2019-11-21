2d9f41ae95403f43ef796a87ab1381e32631cd41
Alexander Mahler <alex.domi.mahler@googlemail.com>
2019-11-21 22:11:47 +0100

    research on json to schema successful. No alternatives needed at the moment therefore there were no alternative libraries evaluated

--- json-schema/main.js
+++ json-schema/main.js
@@ -1,9 +1,19 @@
 var toJsonSchema = require('to-json-schema');
 var jsonPegel = require('./../example-data/01-pegel-stations.json');
 
-var options = {
-    objects: {additionalProperties: false},
+//Options for schema generation
+var options = 
+{
+    objects: {additionalProperties: true},
 }
 
+//Debug
+//console.log(jsonPegel[0]);
+
 var schema = toJsonSchema(jsonPegel[0], options);
+
+//Result
 console.log(schema);
+console.log('************')
+console.log((schema.properties.water));
+
