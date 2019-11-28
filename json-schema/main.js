0c0b45f3149f1a90d8d784d41960d095e37da407
Alexander Mahler <alex.domi.mahler@googlemail.com>
2019-11-28 18:45:26 +0100

    Feature: server functionality extended. Web socket integrated so that python side can receive json data easily

--- json-schema/main.js
+++ json-schema/main.js
@@ -1,5 +1,58 @@
 var toJsonSchema = require('to-json-schema');
 var jsonPegel = require('./../example-data/01-pegel-stations.json');
+var jsonCrime = require('./../example-data/02-crime-data.json');
+var jsonBundestag = require('./../example-data/03-protocol-bundestag.json');
+
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+io.on('connection', client => {   
+    client.on('create-table', data => { 
+        console.log('*** create-table event triggered ***')
+        console.log('data: '+ data)
+
+        if(data == '01')
+        {
+            var schema = toJsonSchema(jsonPegel[0], options);
+            client.emit('create-table', schema, 'pegel');
+        }
+        if(data == '02')
+        {
+            var schema = toJsonSchema(jsonCrime[0], options);
+            client.emit('create-table', schema, 'crime');
+        }
+        /*if(data == '03')
+        {
+            var schema = toJsonSchema(jsonBundestag[0], options);
+            client.emit('create-table', schema, 'bundestag');
+        }*/
+    });
+    client.on('insert-data', (data, index) => { 
+        console.log('*** insert-data event triggered ***')
+        console.log('data: '+ data)
+        console.log('index: '+ index)
+        if(data == '01')
+        {
+            var schema = toJsonSchema(jsonPegel[0], options);
+            client.emit('insert-data',jsonPegel[index], index, schema, 'pegel');
+        }
+        if(data == '02')
+        {
+            var schema = toJsonSchema(jsonCrime[0], options);
+            client.emit('insert-data',jsonCrime[index], index, schema, 'crime');
+        }
+        /*if(data == '03')
+        {
+            var schema = toJsonSchema(jsonBundestag[0], options);
+            client.emit('create-table', schema, 'bundestag');
+        }*/
+    });
+    client.on('disconnect', () => { /* … */ });
+});
+
+
+server.listen(3000);
+console.log('Server listening on port 3000');
 
 //Options for schema generation
 var options = 
@@ -10,10 +63,13 @@ var options =
 //Debug
 //console.log(jsonPegel[0]);
 
-var schema = toJsonSchema(jsonPegel[0], options);
+// ** Does not work with Bundestag weird format **
+//var schema = toJsonSchema(jsonBundestag[0], options);
+
 
 //Result
+/*
 console.log(schema);
 console.log('************')
 console.log((schema.properties.water));
-
+*/
